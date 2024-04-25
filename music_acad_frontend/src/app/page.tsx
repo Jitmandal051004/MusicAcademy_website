a387ba0d2f7a863780906a8b7337a4ab10764bf0
indrajit-mandal <102886330+Jitmandal051004@users.noreply.github.com>
2024-04-25 06:55:09 +0530

    whchooseus and testimonialCards section done

--- music_acad_frontend/src/app/page.tsx
+++ music_acad_frontend/src/app/page.tsx
@@ -1,9 +1,15 @@
+import FeaturedCourses from "@/components/FeaturedCourses";
 import HeroSection from "@/components/HeroSection";
+import MusicSchoolTestimonial from "@/components/TestimonialCards";
+import WhyChooseUs from "@/components/WhyChooseUs";
 
 export default function Home() {
   return (
-    <main className="min-h-screen bg-black/[0.96] antialiased">
+    <main className="min-h-screen bg-black/[0.96] antialiased overflow-hidden">
       <HeroSection />
+      <FeaturedCourses />
+      <WhyChooseUs /> 
+      <MusicSchoolTestimonial />
     </main>
   );
 }
